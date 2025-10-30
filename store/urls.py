97b46fff58eff73d7f4c81f0e3ebd28734b8a91c
sir-3e0 <ewimua@outlook.com>
2025-10-29 23:02:38 -0400

    First commit for mimi-django

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+#from greatkart.urls import urlpatterns
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_details, name='product_details'),
+]
