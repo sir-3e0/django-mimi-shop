edee941f6f93648fc92aaeec45019be3eb540f5b
sir-3e0 <ewimua@outlook.com>
2025-11-01 01:02:36 -0400

    Ending Friday

--- accounts/models.py
+++ accounts/models.py
@@ -41,7 +41,7 @@ class Account(AbstractUser):
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email= models.EmailField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50, unique=True)
 
     #required fields
     date_joined = models.DateTimeField(auto_now_add=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            username = email.split("@")[0]
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password,
+            )
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration is successful')
+            return redirect('register')
+    else:
+        form = RegistrationForm()
+    context = {
+             'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    pass
